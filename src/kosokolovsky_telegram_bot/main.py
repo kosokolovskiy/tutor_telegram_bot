31642b3987b212f59a7463ec9b069b8b9fe06172
kosokolovsky <kosokolovsky@icloud.com>
2025-04-06 21:37:49 +0200

    new

--- src/kosokolovsky_telegram_bot/main.py
+++ src/kosokolovsky_telegram_bot/main.py
@@ -62,12 +62,9 @@ class MyBot:
             print(e)
             message_admin = 'TEST\n' + message
 
-        message = escape_markdown(message, version=2)
-        message_admin = escape_markdown(message_admin, version=2)
         
         await context.bot.send_message(chat_id=chat_id, text=message, parse_mode='MarkdownV2')
         await context.bot.send_message(chat_id=MyBot.get_admin_id(), text=message_admin, parse_mode='MarkdownV2')
-        # await context.bot.send_message(chat_id=972843253, text=message_admin, parse_mode='MarkdownV2')
 
     @staticmethod
     def send_formatted_message(app, chat_id, message):
